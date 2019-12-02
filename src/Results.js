b3f7b47de4b22da635259071733a99d49dc9da52
Jasmine StaRomana <jasmine@tiltedcanvas.com>
2019-12-01 23:12:09 -0500

    eternal struggle, broke my find:cloud:

--- src/Results.js
+++ src/Results.js
@@ -1,49 +1,72 @@
 import React, { Component } from 'react';
+import firebase from './firebase';
 
 class Results extends Component {
+    constructor(){
+        super();
+        this.state = {
+            skateList: [],
+        }   
+    }
+    componentDidMount() {
+        const dbRef = firebase.database().ref();
+        dbRef.on('value', (response) => {
+            const skate = response.val();
+            const newSkate = []
+      for(let key in skate){
+        const eachSkate = {
+          skateId: key,
+          skateDetails: skate[key]
+        }
+        newSkate.push(eachSkate)
+      }
+      console.log(newSkate)
+
+      this.setState ({
+          skateList: newSkate
+      })
+        })
+        
+    }
+
+
+    // getCity = (e, choiceOfCity) => {
+    //     e.preventDefault();
+      
+    //     const originalArray = [...this.state.skateList];
+        
+    //     const skateList = originalArray.filter ( (cityOfSkate) => {
+    //       return eachSkate[choiceOfCity] === true;
+    //     });
+      
+    //     const skateList = skateList.filter( (cityOfEvent) => {
+    //       return skateList.cityOfEvent = '';
+    //     });
+      
+    //     this.setState({
+    //       eachSkate: cityOfSkate
+    //     });
+
+
+   
     render (){
         return (
             <div id='resultsPopup' className='resultsPopup'>
                 <h2>Skates in your City</h2>
                 <div className='resultsContent'>
                     <ul>
-                        <li className='eachResult'>
-                            <h4>Toronto Roller Derby</h4>
+                        {this.state.skateList.map((skateValue, i) => {
+                            console.log('test');
+                        return (
+                            <li key={i} className='eachResult'>
+                            <h4>name</h4>
                             <ul className='flexList'>
                                 <li><span className='infoTitle'>Type of Skate: </span> Roller derby practice</li>
-                                <li><span className='infoTitle'>Date: </span>12-22-2019</li>
-                                <li><span className='infoTitle'>Time: </span>10am to 2pm </li>
-                                <li><span className='infoTitle'>Address: </span>75 Carl Hall Road Unit 15 Toronto, ON M4S2T5</li>
-                                <li><span className='infoTitle'>Cost: </span> $0</li>
-                                <li><span className='infoTitle'>Notes: </span>Bring valid insurance as well as black and white</li>
                             </ul>
                             <button>Contact</button>
-                        </li>
-                        <li className='eachResult'>
-                            <h4>Toronto Roller Derby</h4>
-                            <ul className='flexList'>
-                                <li><span className='infoTitle'>Type of Skate: </span> Roller derby practice</li>
-                                <li><span className='infoTitle'>Date: </span>12-22-2019</li>
-                                <li><span className='infoTitle'>Time: </span>10am to 2pm </li>
-                                <li><span className='infoTitle'>Address: </span>75 Carl Hall Road Unit 15 Toronto, ON M4S2T5</li>
-                                <li><span className='infoTitle'>Cost: </span> $0</li>
-                                <li><span className='infoTitle'>Notes: </span>Bring valid insurance as well as black and white</li>
-                            </ul>
-                            <button>Contact</button>
-                        </li>
-                        <li className='eachResult'>
-                            <h4>Toronto Roller Derby</h4>
-                            <ul className='flexList'>
-                                <li><span className='infoTitle'>Type of Skate: </span> Roller derby practice</li>
-                                <li><span className='infoTitle'>Date: </span>12-22-2019</li>
-                                <li><span className='infoTitle'>Time: </span>10am to 2pm </li>
-                                <li><span className='infoTitle'>Address: </span>75 Carl Hall Road Unit 15 Toronto, ON M4S2T5</li>
-                                <li><span className='infoTitle'>Cost: </span> $0</li>
-                                <li><span className='infoTitle'>Notes: </span>Bring valid insurance as well as black and white</li>
-                            </ul>
-                            <button>Contact</button>
-                    
-                        </li>
+                            </li>
+                        )
+                        })};
                     </ul>
                 </div>
                 <button onClick={this.props.resetForm} className='resetForm'>Return to Search</button>
